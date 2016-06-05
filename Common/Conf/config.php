554acef026d0e9be955adf821830ed2a32c99494
c2002 <libinstar126@126.com>
2016-06-05 22:48:15 +0800

    config file

---
+++ Common/Conf/config.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+
+	'DEFAULT_MODULE'     => 'Home', //默认模块
+	'URL_MODEL'          => '2', //URL模式
+	'SESSION_AUTO_START' => true, //是否开启session
+	'LOAD_EXT_CONFIG'    => 'db',
+	// 'TMPL_ENGINE_TYPE' =>'think',
+	'TMPL_FILE_DEPR'=>'_',
+
+	'MODULE_ALLOW_LIST'    =>    array('Home','Admin'),
+	'DEFAULT_MODULE'       =>    'Home',
+
+	'TMPL_PARSE_STRING'=>array(
+		'__PUBLIC__'=> '/Admin/Public',
+		'__TMPL__' => MODULE_PATH.'View/'
+	),
+
+	'TMPL_L_DELIM'=>'{:',
+	'TMPL_R_DELIM'=>':}',
+
+);
