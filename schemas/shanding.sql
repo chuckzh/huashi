cab0f5edb4f41e67a73d3c78709877f7d8d668c5
c2002 <libinstar126@126.com>
2016-06-06 08:28:36 +0800

    sql backup

---
+++ schemas/shanding.sql
@@ -0,0 +1,56 @@
+/*
+SQLyog Community v12.16 (64 bit)
+MySQL - 5.5.36-log : Database - shanding
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shanding` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `shanding`;
+
+/*Table structure for table `admin_user` */
+
+DROP TABLE IF EXISTS `admin_user`;
+
+CREATE TABLE `admin_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) NOT NULL DEFAULT '',
+  `user_password` char(32) NOT NULL DEFAULT '',
+  `actions` text NOT NULL,
+  `last_login` int(10) unsigned NOT NULL DEFAULT '0',
+  `last_ip` char(15) NOT NULL DEFAULT '',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `admin_user` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) NOT NULL DEFAULT '',
+  `user_password` char(32) NOT NULL DEFAULT '',
+  `phone` char(16) NOT NULL DEFAULT '',
+  `last_login` int(10) unsigned NOT NULL DEFAULT '0',
+  `last_ip` char(15) NOT NULL DEFAULT '',
+  `create_time` int(10) unsigned NOT NULL DEFAULT '0',
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
